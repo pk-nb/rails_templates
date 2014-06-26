1b2aebe87d369a417e0441ae4757e4a7bd16c826
Nathanael Beisiegel <pknb.dev@gmail.com>
2014-06-26 15:33:59 -0600

    Improve generated Guardfile

--- pknb-rails_template.rb
+++ pknb-rails_template.rb
@@ -33,6 +33,7 @@ guard 'livereload' do
   watch(%r{app/views/.+\.(erb|haml|slim)$})
   watch(%r{app/helpers/.+\.rb})
   watch(%r{public/.+\.(css|js|html|scss)})
+  watch(%r{(app|vendor)(/assets/\w+/.+)\.s(c|a)ss}) { |m| "#{m[2]}.css" }
   watch(%r{config/locales/.+\.yml})
   # Rails Assets Pipeline
   watch(%r{(app|vendor)(/assets/\w+/(.+\.(css|js|html|png|jpg))).*}) { |m| "/assets/#{m[3]}" }
@@ -42,6 +43,7 @@ guard 'rails' do
   watch('Gemfile.lock')
   watch(%r{^(config|lib)/.*})
 end
+
 
 EOF
 end
